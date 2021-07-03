967f7903b2e69ab77e07482da83a809cc99d9485
MayaNurani <maya.nurani@theculturetrip.com>
2021-07-03 17:29:12 +0300

    data preparation

--- main.py
+++ main.py
@@ -6,12 +6,31 @@ import pandas as pd
 
 # Part A ex. 1 - Reading file content to data frame
 try:
-    flights_df = pd.read_csv('flights.csv')
+    flights_clustering_df = pd.read_csv('flights.csv')
 except:
     print("Failed to read the file")
-    flights_df = []  # In case the file is not read
+    flights_clustering_df = []  # In case the file is not read
 
 # Data Understanding
-print("There are", flights_df.shape[0], "rows and", flights_df.shape[1], "columns in this file.")
-print("Columns names are: ", list(flights_df.columns))
-print(flights_df.describe())
+print("There are", flights_clustering_df.shape[0], "rows and", flights_clustering_df.shape[1], "columns in this file.")
+print("Columns names are: ", list(flights_clustering_df.columns))
+print(flights_clustering_df.describe())
+
+# unique source and destination flights
+print(flights_clustering_df['Source'].unique())
+print(flights_clustering_df['Destination'].unique())
+print(flights_clustering_df.groupby(['Source', 'Destination']).size().reset_index().rename(columns={0: 'count_unique'}))
+
+
+### Data Preparation ###
+
+# Removing null rows
+if (flights_clustering_df.isnull().values.any()):
+    print("Print columns that contain NaN values", flights_clustering_df.columns[flights_clustering_df.isnull().any()].tolist())
+    flights_clustering_df = flights_clustering_df.dropna()
+else:
+    print('There is no empty values in this dataframe')
+
+# duplicate df for clustering and classification df
+flights_class_df = flights_clustering_df.copy()
+
