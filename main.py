ded444e1ec6b4a42648a37c3289e7d984675d331
Laly <cptlaly@gmail.com>
2021-07-05 20:46:13 +0300

    added averages graphs, changed dates and duration formats

--- main.py
+++ main.py
@@ -15,16 +15,44 @@ except:
     print("Failed to read the file")
     flights_clustering_df = []  # In case the file is not read
 
-# Data Understanding
+### Data Understanding ###
 print("There are", flights_clustering_df.shape[0], "rows and", flights_clustering_df.shape[1], "columns in this file.")
 print("Columns names are: ", list(flights_clustering_df.columns))
 print(flights_clustering_df.describe())
 
+months_dict = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June",
+               "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December"}
+
+
+def change_date_format(datestr):
+    date_lst = datestr.split('/')
+    month = date_lst[1]
+    month = months_dict[month]
+    return month
+
+
+# Adding a column to our df which contains the date month
+flights_clustering_df['Date Month'] = flights_clustering_df['Date'].apply(lambda y: change_date_format(y))
+print(flights_clustering_df)
+
+# Avg ticket price per month
+flights_clustering_df.groupby(['Date Month']).mean().plot(kind='bar', color='red', rot=45)
+plt.title('Average ticket price per month')
+plt.grid()
+plt.show()
+
+# Range of price
+min_price = flights_clustering_df['Price'].min()
+max_price = flights_clustering_df['Price'].max()
+
+# Avg ticket price per company
+flights_clustering_df.groupby(['Airline']).mean().plot(kind='bar', color='orange', rot=45)
+plt.title('Average ticket price per company')
+plt.grid()
+plt.show()
+
 # unique source and destination flights
-print(flights_clustering_df['Source'].unique())
-print(flights_clustering_df['Destination'].unique())
 unique_flights_df = flights_clustering_df.groupby(['Source', 'Destination']).size().reset_index().rename(columns={0: 'count_unique'})
-print(unique_flights_df)
 
 unique_labels = []
 
@@ -35,17 +63,28 @@ def unique_flights_labels():
     unique_labels.append(strlabel)
     return
 
-
+# unique flights pie chart
 unique_count = np.asarray(unique_flights_df['count_unique'])
 unique_flights_labels()
-print(unique_labels)
-print(unique_count)
 plt.pie(unique_count, labels=unique_labels[0], autopct='%1.2f%%')
+plt.title('Unique flights pie chart')
 plt.show()
 
-# Range of price
-min_price = flights_clustering_df['Price'].min()
-max_price = flights_clustering_df['Price'].max()
+
+# Changing duration time format
+def time_format(str):
+    str = str.replace('h', '')
+    str = str.replace('m', '')
+    time_lst = str.split()
+    time_lst[0] = int(time_lst[0])*60
+    if len(time_lst)>1:
+        total_time = int(time_lst[0])+int(time_lst[1])
+    else:
+        total_time = int(time_lst[0])
+    return total_time
+
+flights_clustering_df['Duration Minutes'] = flights_clustering_df['Duration'].apply(lambda x: time_format(x))
+print(flights_clustering_df[['Duration', 'Duration Minutes']])
 
 ### Data Preparation ###
 
